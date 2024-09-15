fae6ce020f89eac6c65b0a1cffa9da7954619145
Toukir Ahemed <toukir.ahemd@dmoneybd.local>
2024-09-15 18:28:35 +0600

    initial commit

---
+++ src/app/components/designation/designation.component.ts
@@ -0,0 +1,24 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { MasterService } from '../../services/master.service';
+import { APIResponseModel, IDesignation } from '../../model/interface/role';
+
+@Component({
+  selector: 'app-designation',
+  standalone: true,
+  imports: [],
+  templateUrl: './designation.component.html',
+  styleUrl: './designation.component.css'
+})
+export class DesignationComponent implements OnInit {
+
+  designationList: IDesignation[] = [];
+  masterService = inject(MasterService);
+
+  ngOnInit(): void {
+    this.masterService.getDesignations().subscribe((result:APIResponseModel)=>{
+      this.designationList = result.data;
+    }, error=>{
+      alert("API error/ Network Down")
+    })
+  }
+}
